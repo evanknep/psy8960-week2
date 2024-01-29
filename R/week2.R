d7920a94573be145eb2663f35b0cb0caa1f5a5ba
evanknep <knepx001@umn.edu>
2024-01-29 14:06:34 -0600

    added levels to fix factorized variables

--- R/week2.R
+++ R/week2.R
@@ -1,11 +1,11 @@
 #Import and Labeling
 rt_df <- read.csv('data/week2.csv', header = TRUE)
-rt_df$condition <- factor(rt_df[,'condition'], labels = c('Control', 'Experimental'))
-rt_df$gender <- factor(rt_df[,'gender'], labels = c('Male', 'Female', 'Nonbinary'))
+rt_df$condition <- factor(rt_df[,'condition'], levels = c('A', 'B'), labels = c('Control', 'Experimental'))
+rt_df$gender <- factor(rt_df[,'gender'], levels = c('M', 'F', 'N'), labels = c('Male', 'Female', 'Nonbinary'))
 
 #Analysis
 mean(rt_df$rt)
 rt_f_df <- subset(rt_df, subset = gender == 'Female')
-hist(rt_f_df$rt)
 datasets <- list(rt_df, rt_f_df)
+hist(rt_f_df$rt)
 datasets[[1]]['rt']
